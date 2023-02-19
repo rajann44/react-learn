b1fddba51720ddc55bf7de1a76d666964635f011
Rajan Chaudhary <rajann44@gmail.com>
2023-02-19 01:01:13 +0100

    improve dark mode and use more props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 
 function App() {
+
+  const[mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#0B243B';
+    }else{
+    setMode('light')
+    document.body.style.backgroundColor = 'white';
+  }
+  }
+
   return (
     <>
-    <Navbar title='Textutils'/>
+    <Navbar title='Textutils' mode={mode} toggleMode={toggleMode}/>
+    <Textform title='Enter your text here:' mode={mode}></Textform>
     <About></About>
-    {/*<Textform title='Enter your text here:'></Textform>*/}
     </>
   );
 }
